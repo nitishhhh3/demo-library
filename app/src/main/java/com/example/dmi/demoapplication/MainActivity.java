75ad4bc9e5c7fec9f7b9d6d698c78544d3d70dea
nsrivastava-dminc-com <nsrivastava@dminc.com>
2016-09-13 12:33:50 +0530

    project created

---
+++ app/src/main/java/com/example/dmi/demoapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.dmi.demoapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.libraryproject.dmi.demolibrary.RssFeedProvider;
+import com.libraryproject.dmi.demolibrary.RssItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        textview = (TextView) findViewById(R.id.textview);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                updateDetail();
+
+            }
+        });
+    }
+
+    public void updateDetail() {  //
+        List<RssItem> list = RssFeedProvider
+                .parse("http://www.vogella.com/article.rss");
+        String text = String.valueOf(list.toString());
+        textview.setText("GOT::" + text);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ demolibrary/src/main/java/com/libraryproject/dmi/demolibrary/RssFeedProvider.java
@@ -0,0 +1,24 @@
+package com.libraryproject.dmi.demolibrary;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by dmi on 9/4/2016.
+ */
+public class RssFeedProvider {
+    public static List<RssItem> parse(String rssFeed) {
+        List<RssItem> list = new ArrayList<>();
+        Random r = new Random();
+        // random number of item but at least 5
+        Integer number = r.nextInt(10) + 5;
+        for (int i = 0; i < number; i++) {
+            // create sample data
+            String s = String.valueOf(r.nextInt(1000));
+            RssItem item = new RssItem("Summary " + s, "Description " + s);
+            list.add(item);
+        }
+        return list;
+    }
+}
